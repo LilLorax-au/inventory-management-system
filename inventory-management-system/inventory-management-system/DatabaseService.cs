d5742a43c9e05b51949a6dd003199b976c5c3269
LilLorax-au <isaac.brown154@outlook.com>
2026-04-15 21:03:59 +0930

    Started Database management class.

---
+++ inventory-management-system/inventory-management-system/DatabaseService.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace inventory_management_system
+{
+    internal class DatabaseService
+    {
+        private const String DATABASE_PATH = "cme_inventory.db";
+
+        private static DatabaseService instance;
+        private SqliteConnection connection;
+
+
+
+        public static DatabaseService Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new DatabaseService();
+                }
+                return instance;
+            }
+        }
+
+        private DatabaseService()
+        {
+            Initialize();
+        }
+
+        private void Initialize() 
+        {
+            connection = new SqliteConnection(DATABASE_PATH);
+        }
+
+        private void SchemeBuilder()
+        {
+
+        }
+    }
+}
